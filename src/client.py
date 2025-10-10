0803393eddbc3ece11ea525462b89aab5c4c5d5d
aem2231 <mariojetullaji@gmail.com>
2025-10-10 02:07:59 +0100

    actual initial commit

--- src/client.py
+++ src/client.py
@@ -1,28 +1,27 @@
 import discord
 from discord.ext import commands
-import config as config
+import config
 import os
 
 cogs_path = "./src/cogs"
 token = config.discordToken
+giphyToken = config.giphyToken
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(intents=intents, command_prefix="?", description="I just got so emo I fell apart.")
 
 def config():
-    if os.path.exists("data/") == False:
+    if not os.path.exists("data/"):
         os.mkdir("data")
-
 config()
 
 @client.event
 async def on_ready():
     print("Client is running!")
-    await loadCogs()
+    for guild in client.guilds:
+        for channel in guild.text_channels:
+            if channel.name == 'general':
+                await channel.send('Hello world!')
+                break
 
-#try:
 client.run(token)
-#except:
-    #print("Plase paste your tokens and api keys in the tokens.json file")
-
-#i use nixOS btw
