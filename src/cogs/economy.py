04865942bcb5085877b5617b37e52b5400245322
aem2231 <mariojetullaji@gmail.com>
2025-10-12 01:00:48 +0100

    completed database functions

--- src/cogs/economy.py
+++ src/cogs/economy.py
@@ -1,15 +1,13 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from discord.types.interactions import Interaction
-import random
-import sqlite3
-import requests
+from Database import Database
 
 class Economy(commands.Cog):
   group = app_commands.Group(name="economy", description="Economy commands")
   def __init__(self, bot: commands.Bot) -> None:
     self.bot: commands.Bot = bot
+    self.db = Database()
 
   @group.command(name="work", description="Work a random job for some dabloons.")
   async def work(self, inter: discord.Interaction) -> None:
@@ -17,10 +15,6 @@ class Economy(commands.Cog):
 
   @group.command(name="gamble", description="Gamble some money.")
   async def gamble(self, inter: discord.Interaction) -> None:
-    _ = await inter.response.send_message("working")
-
-  @group.command(name="buy", description="Buy something")
-  async def buy(self, inter: discord.Interaction) -> None:
     _ = await inter.response.send_message("working")
 
   @group.command(name="rob-bank", description="Rob the bank!")
