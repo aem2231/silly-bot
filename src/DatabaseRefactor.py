bf65def7ac38cf65a4a3a6574e147633b28091d7
aem2231 <mariojetullaji@gmail.com>
2025-10-14 00:26:23 +0100

    refactored database functions, started to refactor economy commands

--- src/DatabaseRefactor.py
+++ src/DatabaseRefactor.py
@@ -10,9 +10,8 @@ def db_cursor(func):
     try:
       result = func(self, cursor, *args, **kwargs)
       self.conn.commit()
-      print('hi')
       return result
-    except sqlite3.OperationalError as e:
+    except Exception as e:
       print(f"Database error: {e}")
       self.conn.rollback()
       return None
@@ -21,14 +20,12 @@ def db_cursor(func):
 class EconomyDatabase():
   def __init__(self):
     self.db_path = Path("db1/database.db")
+    self.user_table = "users"
+    self.guild_table = "server"
+    self.user_column = "user_id"
+    self.guild_column = "guild_id"
     self.conn = sqlite3.connect(self.db_path)
     self.db_setup()
-
-  def calculate_remaining_cooldown(self, cooldown_length, cooldown_start_unix_timestamp):
-    current_time = datetime.datetime.now().timestamp()
-    if cooldown_length + cooldown_start_unix_timestamp < current_time:
-      return True
-    return False
 
   @db_cursor
   def db_setup(self, cursor):
@@ -54,24 +51,51 @@ class EconomyDatabase():
     ]
 
     for statement in setup_statements:
-      print(statement)
       cursor.execute(statement)
 
+  def calculate_remaining_cooldown(self, cooldown_length, cooldown_start_unix_timestamp):
+    current_time = datetime.datetime.now().timestamp()
+    if cooldown_length + cooldown_start_unix_timestamp < current_time:
+      return 0
+    return (cooldown_start_unix_timestamp + cooldown_length) - current_time
+
   @db_cursor
-  def check_cooldown(self, cursor, task, table, cooldown_length, cooldown_start_unix_timestamp, id):
-    statement = f"SELECT f{task} FROM {table} WHERE {id} = :id"
+  def check_cooldown(self, cursor, task, table, cooldown_length, id_column, id):
+    self.check_entity_presence(cursor, table, id_column, id)
+    statement = f"SELECT {task} FROM {table} WHERE {id_column} = :id"
     cursor.execute(statement, {"id": id})
+    row = cursor.fetchone()
+    if row is None:
+      return 0
+    cooldown_start_time = row[0]
+    if cooldown_start_time is None:
+      return 0
+    remaining_cooldown_seconds = self.calculate_remaining_cooldown(cooldown_length, cooldown_start_time)
+    return remaining_cooldown_seconds
 
   @db_cursor
-  def update_coins(self, id, coins):
-    pass
+  def update_coins(self, cursor, id, coins):
+    self.check_entity_presence(cursor, self.user_table, self.user_column, id)
+    statement = f"UPDATE users SET balance = :coins WHERE user_id = :id"
+    cursor.execute(statement, {"coins": coins, "id": id})
+    return True
 
   @db_cursor
-  def get_balance(self, id):
-    pass
+  def get_balance(self, cursor, id):
+    self.check_entity_presence(cursor, self.user_table, self.user_column, id)
+    statement = f"SELECT balance FROM users WHERE user_id = :id"
+    cursor.execute(statement, {"id": id})
+    row = cursor.fetchone()
+    return row[0]
 
-  @db_cursor
-  def check_entity_presence(self, entity_type, id):
-    pass
+  def check_entity_presence(self, cursor, table, id_column, id_value):
+    statement = f"SELECT 1 FROM {table} WHERE {id_column} = :id_value LIMIT 1"
+    cursor.execute(statement, {"id_value": id_value})
+    row = cursor.fetchone()
 
-db1 = EconomyDatabase()
+    if row is None:
+      self.add_entity(cursor, table, id_column, id_value)
+
+  def add_entity(self, cursor, table, id_column, id_value):
+    statement = f"INSERT INTO {table} ({id_column}) VALUES (:id_value)"
+    cursor.execute(statement, {"id_value": id_value})
--- src/cogs/economy.py
+++ src/cogs/economy.py
@@ -1,15 +1,27 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from Database import Database
+from DatabaseRefactor import EconomyDatabase
 import random
 import time
 
 class Economy(commands.Cog):
   group = app_commands.Group(name="economy", description="Economy commands")
   def __init__(self, bot: commands.Bot) -> None:
-    self.bot: commands.Bot = bot
-    self.db = Database()
+    # id types and cooldowns to be passed to database functions when needed
+    self.user_column = "user_id"
+    self.guild_column = "guild_id"
+    self.user_table = "users"
+    self.guild_table = "server"
+    self.daily_column = "last_daily"
+    self.bank_rob_columnn = "last_bank_rob"
+    self.work_column = "last_work"
+    self.cooldowns = {
+      "bank_rob": 1800,
+      "work": 21600,
+      "daily": 86400
+    }
+
     self.job_list = [
       "cleaner",
       "cashier",
@@ -21,200 +33,46 @@ class Economy(commands.Cog):
       "bus driver"
     ]
 
+    self.bot: commands.Bot = bot
+    self.db = EconomyDatabase()
+
+
+  def build_embed(self, embed_title, embed_description, embed_color):
+    embed = discord.Embed(
+      title=embed_title,
+      description=embed_description,
+      color=embed_color
+    )
+    return embed
+
+  @group.command(name="daily", description="Claim your daily coins!")
+  async def daily(self, inter: discord.Interaction) -> None:
+    remaining_cooldown = self.db.check_cooldown(self.daily_column, self.user_table, self.cooldowns["daily"], self.user_column, inter.user.id)
+    if remaining_cooldown == 0:
+      _ = await inter.response.send_message("daily allowed")
+      return
+    _ = await inter.response.send_message("daily blocked")
+
   @group.command(name="work", description="Work a random job for some dabloons.")
   async def work(self, inter: discord.Interaction) -> None:
-
-    embed = discord.Embed(
-      title="Work",
-      description=""
-    )
-
-    can_work = self.db.check_work(inter.user.id)
-    if can_work is None:
-      embed.description = "Database error occurred. Please try again later."
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-    # could be moved to a helper function to avoid repetition: calculate_remaining_cooldown(cooldown, cooldown_start_unix_timestamp)
-    # also found on lines 77-85 and 165-171
-    if can_work[0] == False:
-      six_hours_seconds = 21600
-      time_since_last_work = can_work[1]
-      time_to_work = six_hours_seconds - time_since_last_work
-
-      time_to_work = max(0, time_to_work)
-
-      hours_to_work = time_to_work // 3600
-      minutes_to_work = (time_to_work % 3600) // 60
-
-      embed.description=f"You cannot work for another {int(hours_to_work)}hrs {int(minutes_to_work)}m"
-      _ = await inter.response.send_message(embed=embed)
-    else:
-      payout = random.randint(100, 250)
-      balance = self.db.get_balance(inter.user.id)
-      job = self.job_list[random.randint(0, (len(self.job_list)-1))]
-      if balance == None:
-        _ = await inter.response.send_message("Something went wrong :(")
-        return
-      new_balance = payout+balance
-      self.db.update_coins(inter.user.id, new_balance)
-      embed.description=f"You worked as as {job} and got paid {payout}! Your balance is now {new_balance}"
-      _ = await inter.response.send_message(embed=embed)
-
-  @group.command(name="daily", description="Claim daily reward")
-  async def daily(self, inter: discord.Interaction) -> None:
-    embed = discord.Embed(
-      title="Daily",
-      description=""
-    )
-
-    can_claim_daily = self.db.check_daily(inter.user.id)
-    if can_claim_daily == None:
-      embed.description = "Database error occured. Please try again later."
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-    if can_claim_daily[0] == False:
-      day_seconds = 86400
-      time_since_last_daily = can_claim_daily[1]
-      time_to_daily = day_seconds - time_since_last_daily
-
-      time_to_daily = max(0, time_to_daily)
-
-      hours_to_daily = time_to_daily // 3600
-      minutes_to_daily = (time_to_daily % 3600) // 60
-
-      embed.description=f"You already claimed your daily today. Claim your next daily in {int(hours_to_daily)}hrs {int(minutes_to_daily)}m"
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-    else:
-      payout = 1000
-      balance = self.db.get_balance(inter.user.id)
-      if balance == None:
-        _ = await inter.response.send_message("Something went wrong, try again later.")
-        return
-      new_balance = balance + payout
-      self.db.update_coins(inter.user.id, new_balance)
-      embed.description=f"You claimed {payout}. Your new balance is {new_balance}"
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-  @group.command(name="gamble", description="Gamble some money.")
-  @app_commands.describe(amount="amount")
-  async def gamble(self, inter: discord.Interaction, amount: int) -> None:
-    balance = self.db.get_balance(inter.user.id)
-    chances = (True, False)
-
-    embed = discord.Embed(
-      title="Gambling",
-      description=""
-    )
-
-    if balance == None:
-      _ = await inter.response.send_message("Something went wrong, try again later.")
-      return
-
-    if amount > balance:
-      embed.description= f"You cannot gamble more than your balance. Your balance is {balance} coins."
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-    win = chances[random.randint(0,1)]
-    if win:
-      winnings = amount*2
-      new_balance = winnings + balance
-      self.db.update_coins(inter.user.id, new_balance)
-      embed.description = f"You won {winnings} coins! Your new balance is {new_balance}."
-      _ = await inter.response.send_message(embed=embed)
-      return
-
-    new_balance = balance - amount
-    self.db.update_coins(inter.user.id, new_balance)
-    embed.description = f"You lost {amount} coins. Your new balance is {new_balance}"
-    _ = await inter.response.send_message(embed=embed)
-
-  @group.command(name="rob-bank", description="Rob the bank!")
-  async def rob_bank(self, inter: discord.Interaction) -> None:
-    chances = [False, False, False, False, False, False, False, True, True, True, True] # 0 means not caught
-    caught = chances[random.randint(0,10)]
-    payout = random.randint(5000, 10000)
-    can_bank_rob = self.db.check_bank(inter.guild.id)
-    balance = self.db.get_balance(inter.user.id)
-
-    embed = discord.Embed(
-      title="Bank rob",
-      description=""
-    )
-
-    if can_bank_rob == None or balance == None:
-      _ = await inter.response.send_message("Something went wrong, try again later.")
-      return
-
-    if can_bank_rob[0] and not caught:
-      new_balance = balance + payout
-      self.db.update_coins(inter.user.id, new_balance)
-      description = f"You stole {payout} coins from the vault! Your new balance is {new_balance}"
-      embed.description=description
-    elif can_bank_rob[0] and caught:
-      fine = balance//10
-      new_balance = balance - fine
-      self.db.update_coins(inter.user.id, new_balance)
-      description = f"You were caught and fined {fine} coins. Your new balance is {new_balance}"
-    else:
-      half_hour_seconds = 1800  # 30 minutes
-
-      last_rob_time = can_bank_rob[1]  # Unix timestamp of last rob
-      now = time.time() # get current time
-      time_since_last_rob = now - last_rob_time
-      time_to_bank = max(0, half_hour_seconds - time_since_last_rob)
-      minutes_to_bank = round(time_to_bank / 60) # convert seconds to minutes
-
-      description = f"The bank has recently been robbed. It can next be robbed in {minutes_to_bank}m"
-
-    embed.description=description
-    _ = await inter.response.send_message(embed=embed)
-
-  @group.command(name="beg", description="Beg for money.")
-  async def beg(self, inter: discord.Interaction) -> None:
-    chances = [True, True, True, False, False]
-    won = chances[random.randint(0,4)]
-    balance = self.db.get_balance(inter.user.id)
-
-    embed = discord.Embed(
-      title="Beg",
-      description=""
-    )
-
-    if balance == None:
-      _ = await inter.response.send_message("Something went wrong, try again later.")
-      return
-
-    if won:
-      coins = random.randint(5,50)
-      new_balance = coins + balance
-      self.db.update_coins(inter.user.id, new_balance)
-      description = f"A kind stranger gave you {coins}! Your new balance is {new_balance}"
-    else:
-      description = "You pretented to be homeless, but no one cared."
-
-    embed.description=description
-    _ = await inter.response.send_message(embed=embed)
+    pass
 
   @group.command(name="balance", description="Check your balance.")
   async def balance(self, inter: discord.Interaction) -> None:
-    balance = self.db.get_balance(inter.user.id)
-    if balance == None:
-      description = "Something went wrong, try again later."
-    else:
-      description = f"Your balance is {balance} coins."
+    pass
 
-    embed = discord.Embed(
-      title="Balance",
-      description=description
-    )
+  @group.command(name="beg", description="Beg for money.")
+  async def beg(self, inter: discord.Interaction) -> None:
+    pass
 
-    _ = await inter.response.send_message(embed=embed)
+  #@group.command(name="coinflip", description="Flip a coin for a chance to double your money!")
+ # @app_commands.describe(amount="Amount of money to gamble.")
+  #async def coinflip(self, inter: discord.Interaction) -> None:
+  #  pass
+
+  @group.command(name="bankrob", description="Try and rob the bak for some coins!")
+  async def bankrob(self, inter: discord.Interaction) -> None:
+    pass
 
 async def setup(bot: commands.Bot) -> None:
   await bot.add_cog(Economy(bot))
