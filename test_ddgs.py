44b03429dec0746bcb9ea9249fd642381aea50be
aem2231 <mariojetullaji@gmail.com>
2026-04-04 01:03:53 +0100

    i hate my life

--- test_ddgs.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from ddgs import DDGS
-    print("Successfully imported DDGS from ddgs")
-    results = DDGS().images("cats", max_results=2)
-    print("Results:", list(results))
-except Exception as e:
-    print("Error:", type(e), e)
