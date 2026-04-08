06bd77be8f156b8513f00dac7eee5c16f2f0af59
aem2231 <mariojetullaji@gmail.com>
2026-04-08 21:12:39 +0100

    added wrapper fucnion to handle database sessions

--- src/cogs/levelling.py
+++ src/cogs/levelling.py
@@ -1,11 +1,9 @@
-from zlib import MAX_WBITS
 import discord
 from discord.ext import commands
 from discord import app_commands
-from db.database import AsyncSessionLocal
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.database import with_db
 from db import crud
-from core import constants as const
-import random
 
 class Levelling(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -32,55 +30,52 @@ class Levelling(commands.Cog):
         return embed
 
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
+    @with_db
+    async def on_message(self, message: discord.Message, db: AsyncSession):
         if message.author.bot:
             return
 
         user_id = str(message.author.id)
-        print(message.content)
 
-        async with AsyncSessionLocal() as session:
-            if not await crud.get_user_by_id(session, user_id):
-                await crud.register_user(session, user_id, 0)
-                current_level = await crud.get_user_level(session, user_id)
-                current_xp = await crud.get_user_xp(session, user_id)
+        if not await crud.get_user_by_id(db, user_id):
+            await crud.register_user(db, user_id, 0)
 
-            current_level = await crud.get_user_level(session, user_id)
-            current_xp = await crud.get_user_xp(session, user_id)
+        current_level = await crud.get_user_level(db, user_id)
+        current_xp = await crud.get_user_xp(db, user_id)
 
-            multiplier = self.calculate_multiplier(current_level)
-            xp_increase = self.calculate_xp_increase(message.content, multiplier)
-            new_xp = current_xp + xp_increase
+        multiplier = self.calculate_multiplier(current_level)
+        xp_increase = self.calculate_xp_increase(message.content, multiplier)
+        new_xp = current_xp + xp_increase
 
-            xp_needed = self.calculate_next_level_xp(current_level)
+        xp_needed = self.calculate_next_level_xp(current_level)
 
-            if self.check_level_up(new_xp, xp_needed):
-                new_level = current_level + 1
-                await crud.update_user_level(session, user_id, new_level)
-                await crud.update_user_xp(session, user_id, new_xp)
+        if self.check_level_up(new_xp, xp_needed):
+            new_level = current_level + 1
+            await crud.update_user_level(db, user_id, new_level)
+            await crud.update_user_xp(db, user_id, new_xp)
 
-                embed = await self.get_level_up_embed(user_id, new_level)
-                await message.channel.send(embed=embed)
-            else:
-                await crud.update_user_xp(session, user_id, new_xp)
+            embed = await self.get_level_up_embed(user_id, new_level)
+            await message.channel.send(embed=embed)
+        else:
+            await crud.update_user_xp(db, user_id, new_xp)
 
     @app_commands.command(name="level", description="View your level and XP")
-    async def level(self, inter: discord.Interaction):
+    @with_db
+    async def level(self, inter: discord.Interaction, db: AsyncSession):
         user_id = str(inter.user.id)
 
-        async with AsyncSessionLocal() as session:
-            level = await crud.get_user_level(session, user_id)
-            xp = await crud.get_user_xp(session, user_id)
+        level = await crud.get_user_level(db, user_id)
+        xp = await crud.get_user_xp(db, user_id)
         embed = discord.Embed(title=f"{inter.user.name}'s Level", description=f"Level: {level}\nXP: {xp}\n{xp}/{self.calculate_next_level_xp(level)} for next level.")
         await inter.response.send_message(embed=embed)
 
     @app_commands.command(name="leaderboard", description="View the leaderboard")
-    async def leaderboard(self, inter: discord.Interaction):
-        async with AsyncSessionLocal() as session:
-            users = await crud.get_top_users_global(session, 10)
-            leaderboard = "\n".join([f"{i+1}. <@{user.user_id}> - Level {user.level}" for i, user in enumerate(users)]) # update db to store usernames eventually
-            embed = discord.Embed(title="Leaderboard", description=leaderboard)
-            await inter.response.send_message(embed=embed)
+    @with_db
+    async def leaderboard(self, inter: discord.Interaction, db: AsyncSession):
+        users = await crud.get_top_users_global(db, 10)
+        leaderboard = "\n".join([f"{i+1}. <@{user.user_id}> - Level {user.level}" for i, user in enumerate(users)]) # update db to store usernames eventually
+        embed = discord.Embed(title="Leaderboard", description=leaderboard)
+        await inter.response.send_message(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
